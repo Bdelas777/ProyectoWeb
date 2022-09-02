60a39b61b5613cf5e3e50550d3eac58f83aed1b3
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2022-09-02 10:57:35 -0500

    Initial commit

---
+++ ProyectoWebapp/urls.py
@@ -0,0 +1,14 @@
+#Archivo para incluir varias urls
+
+from django.urls import path
+from ProyectoWebapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('',views.home, name = "Home"),
+    path('tienda',views.tienda, name = "Tienda"),
+    path('contacto',views.Contacto, name = "Contacto"),
+  
+]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
