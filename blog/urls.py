069b1a560e2356172782b29236faaed5a34ed85c
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2022-09-05 16:35:06 -0500

    Actualiza

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.blog, name = "Blog"),
-    path('categoria/int:<categoria_id>/', views.categoria , name = views.categoria , name = "categoria"),
+    path('categoria/int:<categoria_id>/', views.categoria , name = "categoria"),
   
 ]
