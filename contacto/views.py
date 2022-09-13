c64667655d9cf7a681814a7a5b47229ac914bb37
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2022-09-12 21:30:11 -0500

    Contacto

--- contacto/views.py
+++ contacto/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from .forms import FormularioContacto
 # Create your views here.
 # Uso de la libreria forms
 def contacto(request):
-    
-    return render(request, "contacto/contacto.html")
+    formulario_contacto = FormularioContacto()
+    return render(request, "contacto/contacto.html", {"miformulario": formulario_contacto})
